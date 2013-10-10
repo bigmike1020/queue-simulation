aff147082ba3cb21accbce2a6955cb8e90ae5bb2
Mike Senn <mpsenn@ncsu.edu>
2013-10-09 20:59:15 -0400

    Tuning print methods

--- printer.cpp
+++ printer.cpp
@@ -3,11 +3,10 @@
 #include <cstdlib>
 #include <cstdio>
 
-#define FP_FORMAT "%#4.3f"
-#define INT_FORMAT "%d"
-
 using util::begin;
 using util::end;
+
+const char ITEM_FORMAT[] = "%6.2f(%4.2f)";
 
 const char* str(TransferSpeed speed)
 {
@@ -23,60 +22,91 @@ const char* str(Flag flag)
 	return "";
 }
 
-std::string str(EventType event)
-{
-	std::ostringstream ss;
-	ss << int(event);
-	return ss.str();
-}
-
 void StateHistory::emplace_back(const SimState& state)
 {
 	history.emplace_back(state);
-	/*
-	int size = snprintf(nullptr, 0, FP_FORMAT, state.mcl);
-	mclSize = std::max(mclSize, size);
+}
 
-	size = snprintf(nullptr, 0, FP_FORMAT, state.nextTransfer);
-	clpSize = std::max(clpSize, size);
+void printServerQueue(std::deque<ServerQueueItem>& serverQueue)
+{
+	char serverStr[33] = {'\0'};
+	if(!serverQueue.empty())
+	{
+		auto packet = serverQueue.front();
+		serverQueue.pop_front();
 
-	size = snprintf(nullptr, 0, FP_FORMAT, state.nextConsume);
-	clcqSize = std::max(clpcqSize, size);
+		int chars = snprintf(serverStr, 33, ITEM_FORMAT, 
+			packet.getExitTime(), packet.getDelay());
 
-	size = snprintf(nullptr, 0, FP_FORMAT, state.clientQueue.size());
-	clientQueueSize = std::max(clientQueueSize, size);
-	*/
+		if(!serverQueue.empty())
+		{
+			packet = serverQueue.front();
+			serverQueue.pop_front();
+			snprintf(serverStr+chars, 33-chars, ITEM_FORMAT, 
+				packet.getExitTime(), packet.getDelay());
+		}
+	}
+	
+	printf("|%35s|", serverStr);
 }
 
 void StateHistory::print()
 {
 	printf( 
-"  Actions |  Server   |      Infinite Server Queue       |  Client Queue  \n"
-" MCL|Event|Trans.|CL-p|       CL-isq per waiting         |CL-cq| Νο. |flag\n"
-"    | No  |Speed |    |         packet (delay)           |     |queue|    \n"
+"    Actions  |  Server   |       Infinite Server Queue       |"
+"  Client Queue   \n"
+"   MCL |Event|Trans.|CL-p|        CL-isq per waiting         |"
+"CL-cq | Νο. |flag\n"
+"       | No  |Speed |    |          packet (delay)           |"
+"      |queue|    \n"
 	);
 	
 	for(auto it = begin(history); it != end(history); ++it)
 	{
-		printf("%4f|%5d|%6s|%4f|", it->mcl, int(it->event), str(it->speed),
-			it->nextTransfer);
+		printf(
+"------- ----- ------ ---- ----------------------------------- "
+"------ ----- ----\n"
+		);
 
-		auto& serverQueue = it->serverQueue;
-		for(auto it2 = begin(serverQueue); it2 != end(serverQueue); ++it2)
+		if(it->mcl == TIME_INFINITY)
 		{
-			printf("%4f(%4f) ", it2->getExitTime(), it2->getDelay());
-		}
-		
-		if(it->nextConsume == TIME_INFINITY)
-		{
-			printf("|  -  |    0|");
+			printf("    -  |");
 		}
 		else
 		{
-			printf("|%5f|%5zu|", it->nextConsume, it->clientQueue.size());
+			printf(" %6.2f|", it->mcl);
 		}
-		
+
+		printf("%3d  |%4s  |", int(it->event), str(it->speed));
+
+		if(it->nextTransfer == TIME_INFINITY)
+		{
+			printf("  - ");
+		}
+		else
+		{
+			printf("%3.0f ", it->nextTransfer);
+		}
+
+		std::deque<ServerQueueItem> serverQueue = it->serverQueue;
+		printServerQueue(serverQueue);
+
+		if(it->nextConsume == TIME_INFINITY)
+		{
+			printf("   -  |   0 |");
+		}
+		else
+		{
+			printf("%6.2f|%4zu |", it->nextConsume, it->clientQueue.size());
+		}
+
 		printf("%4s\n", str(it->flag));
-		
+
+		while(!serverQueue.empty())
+		{
+			printf("                         ");
+			printServerQueue(serverQueue);
+			printf("\n");
+		}
 	}
 }
