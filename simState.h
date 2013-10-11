5679278c0b8e409cd44875fc75a8d53310c68d7e
Mike Senn <mpsenn@ncsu.edu>
2013-10-11 16:29:45 -0400

    Inline simState.cpp

--- simState.h
+++ simState.h
@@ -55,6 +55,22 @@ struct SimState
 	// Set to UP or DOWN when client transfers happen
 	Flag flag;
 
-	SimState(const Options& opts);
+	SimState(const Options& opts)
+		: mcl(0.0f)
+		, event(EventType::TRANSFER)
+		, speed(TransferSpeed::HIGH)
+		, nextTransfer(TIME_INFINITY)
+		, nextConsume(TIME_INFINITY)
+		, flag(Flag::NOPRINT)
+	{
+		PacketFactory factory(opts);
+
+		for (int i = 0; i <= opts.packets; ++i)
+		{
+			untransferred.emplace_back(factory());
+		}
+		
+		nextTransfer = TIME_BEGIN;
+	}
 };
 
