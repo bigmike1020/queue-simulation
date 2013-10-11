8f6443e3b84bb1750849e8b997de67bf032bf28d
Mike Senn <mpsenn@ncsu.edu>
2013-10-11 16:27:57 -0400

    Inline packet.cpp

--- packet.h
+++ packet.h
@@ -17,7 +17,13 @@ class Packet
 	TimeDiff server;
 	TimeDiff client;
 	
-	Packet(TimeDiff server, TimeDiff client);
+	Packet(TimeDiff server, TimeDiff client)
+		: server(server), client(client)
+	{
+		assert(server > 0 && "Time spent in queue must be > 0");
+		assert(client > 0 && "Time spent in queue must be > 0");
+	}
+
 public:
 	
 	TimeDiff getServerTime() const {return server;}
@@ -33,6 +39,19 @@ class PacketFactory
 	util::exponential_distribution<TimeDiff> client_dist;
 public:
 	
-	PacketFactory(const Options& opts);
-	Packet operator()();
+	PacketFactory(const Options& opts)
+		: rand_engine(opts.seed)
+		, server_dist(1.0f / opts.meanTimeServerQueue)
+		, client_dist(1.0f / opts.meanTimeClientQueue)
+	{}
+
+	Packet operator()()
+	{
+		auto server = server_dist(rand_engine);
+		auto client = client_dist(rand_engine);
+		
+		Packet p(server, client);
+		return p;
+	}
+
 };
